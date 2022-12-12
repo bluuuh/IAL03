316dfa06380b02bda6b2f5dfe55316dc9ef452f7
Mike <mike.rene.wagner.97@gmail.com>
2022-12-12 19:06:59 +0100

    ++

--- Aufgabe_LS1/aufgabe_1s1_01.py
+++ Aufgabe_LS1/aufgabe_1s1_01.py
@@ -3,8 +3,8 @@
     erstellt am : 22.08.22
     geändert am : 29.08.22
     """
-from description import description
-description("Taschenrechner")
+
+
 operator = input('Rechenoperator eingeben(+,-,*,/):')
 num_1 = float(input('erste Zahl eingeben'))
 num_2 = float(input('zweite Zahl eingeben'))
@@ -12,11 +12,10 @@ num_2 = float(input('zweite Zahl eingeben'))
 if operator == "+":
     print('Ergebsnis:', num_1+num_2)
 elif operator == "-":
-    print('Ergebsnis:',num_1-num_2)
+    print('Ergebsnis:', num_1-num_2)
 elif operator == "*":
-    print('Ergebsnis:',num_1*num_2)
+    print('Ergebsnis:', num_1*num_2)
 elif operator == "/":
-    print('Ergebsnis:',round(num_1/num_2, 2))
+    print('Ergebsnis:', round(num_1/num_2, 2))
 else:
     print('falsche Eingabe')
-
--- Aufgabe_LS1/aufgabe_1s1_02.py
+++ Aufgabe_LS1/aufgabe_1s1_02.py
@@ -1,9 +1,8 @@
 temperature = input('Enter Temperature:')
 temp = temperature.split('°')
-if "C" in temperature:    
-    print(int(temp[0])* 1.8 + 32)
+if "C" in temperature:
+    print(int(temp[0]) * 1.8 + 32)
 elif "F" in temperature:
-    print((int(temp[0])-32)/ 1.8)
+    print((int(temp[0])-32) / 1.8)
 else:
-    print('falsche Eingabe')
-
+    print('falsche Eingabe "zB 70°F"')
